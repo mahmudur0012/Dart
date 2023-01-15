66974f4cefac283109dcc9732eae3c091cf7a4af
mahmudur0012 <mahmudur0012@gmail.com>
2023-01-16 01:16:19 +0600

    variable

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,21 @@
 void main() {
   print("Bismillah");
   
+  //datatype
+  //int- normal number
+
+  int tk = 100;
+
+  //double- pointtype number
+
+  double poysha = 0.98;
+
+  //string- name/taxt
+
+  String name = "Bd";
+
+print(tk);
+print(poysha);
+print(name);
+
 }
