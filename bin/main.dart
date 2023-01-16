bcd583b17fe3f208b0d41e39719e4f1c4f0e893c
mahmudur0012 <mahmudur0012@gmail.com>
2023-01-17 00:46:57 +0600

    interpolation

--- bin/main.dart
+++ bin/main.dart
@@ -14,8 +14,20 @@ void main() {
 
   String name = "Bd";
 
+//literals hoitase belue ! jemon int a = 10 aikhane 10 hoitase literals
+
+// jodi double cotetion 2 bar hoy tahole \ diya cotationte bebohar korte hobe, jemon = 
+String mynote = "sir bipul said,\"you are my murid.\""; 
+
+//interpolation
+
+int a = 100;
+int b = 200;
+
+print("${a} + ${b} = ${a+b}");
+
 print(tk);
 print(poysha);
 print(name);
-
+print(mynote);
 }
