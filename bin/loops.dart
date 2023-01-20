d9a6e5eb052816c79252a50c4f43e02b680ebb42
mahmudur0012 <mahmudur0012@gmail.com>
2023-01-20 23:18:21 +0600

    Loops

--- bin/loops.dart
+++ bin/loops.dart
@@ -2,7 +2,20 @@ void main (){
   int i = 18;
 
   while (i <= 20){
-    print("01762230804");
+    print("hi");
     i = i + 1;
+  }
+
+  do {
+    print("kemon acho"); //exit control Loop
+    i = i +2;
+  } while (i <=5);
+
+  for(var i = 5;i <=5 ; i++ /* increment/ dekrement*/ ){
+    print("bhalo achi");
+  }
+
+  for ( var a = 1,i=1; i<=10; i++){
+    print("${a} X ${i} = ${a*i}");
   }
 }
